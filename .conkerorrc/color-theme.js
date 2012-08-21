144e110c0cec44911458ca50e7bfbe43ef018d72
Scott Jaderholm <jaderholm@gmail.com>
2012-08-20 17:56:36 -0700

    added hacker news dark styles and improved google reader styles

--- .conkerorrc/color-theme.js
+++ .conkerorrc/color-theme.js
@@ -3,21 +3,19 @@
    ------------------------------ */
 
 register_user_stylesheet(
-    make_css_data_uri(["select,option,input { color: #bbb !important; background-color: #333 !important; }"+
-                       "code, pre { font-family: DejaVu Sans Mono !important; "+
-                       "font-size: 12px !important; }"+
-                       "span.__conkeror_hint {"+
-                       " line-height: 14px !important;"+
-                       ' font-family: Dina !important; '+
-                       // ' font-family: Tamsyn !important; '+
-                       // ' font-weight: normal !important;'+
-                       // ' color: green !important; '+
-                       " font-size: 12px !important;"+
-                       "}\n"]));
-
-               /* "#minibuffer, .mode-line, .completions {"+
-                " font-family: Terminus; font-size: 12pt;"+ 
-               "}\n"+ */
+    make_css_data_uri(
+        ["select,option,input { color: #bbb !important; background-color: #333 !important; }"+
+         "code, pre { font-family: DejaVu Sans Mono !important; "+
+         "font-size: 12px !important; }"+
+         "span.__conkeror_hint {"+
+         " line-height: 14px !important;"+
+         ' font-family: Dina !important; '+
+         ' padding: 1px !important;'+
+         // ' font-family: Tamsyn !important; '+
+         // ' font-weight: normal !important;'+
+         // ' color: green !important; '+
+         " font-size: 12px !important;"+
+         "}\n"]));
 
 // toggle dark mode, adapted from philjackson's darken function
 color_theme_toggle = {};
@@ -46,65 +44,82 @@ function global_color_theme(name, key, styles) {
 global_color_theme(
     "dark", "K",
     [make_css_data_uri(
-         [
-             '*:not(img) {'+
-                 'font-style: normal !important;'+ // italics is hard to read with gohu
-                 'background: #303030 !important;'+
-                 'text-shadow: 0px 0px 0px  black !important;'+ // fix ugly shadows
-                 'letter-spacing: 0 !important;'+               // don't scrunch words
-                 // '*:not(img) { background: #262626 !important; '+
-                 // 'color: #f6f3e8 !important; '+
-                 'color: #cccccc !important; '+
-                 // 'color: #686868 !important; '+
-                 // 'font-family: GohuFont, Tamsyn !important; '+
-                 'font-family: Dina !important; '+
-                 // 'font-family: DejaVu Sans !important; '+
-                 // 'font-family: Inconsolata !important; '+
-                 // 'font-family: MonteCarlo !important; '+
-                 // 'font-family: Terminus !important; '+
-                 // 'font-family: Envy Code R !important; '+
-                 'font-size: 10pt !important; '+
-                 '-moz-border-bottom-colors: #444 #444 !important; '+
-                 '-moz-border-top-colors: #444 #444  !important; '+
-                 '-moz-border-right-colors: #444 #444  !important; '+
-                 '-moz-border-left-colors: #444 #444  !important; '+
-                 '-moz-box-shadow: 0 0 0 black !important; '+
-                 'border-color: #444 !important; '+
-                 'text-decoration: none !important;'+
-                 ' }',
-             'p { line-height: 100% !important; }',
-             'code, pre, code *, pre * { color: #f6f3e8 !important; background-color: #353535 !important; }',
-             ':link, :link * {  color: #8ac6f2 !important}',
-             ':visited, :visited * { color: #95e454 !important}',
-             'h1,h2,h3,h4,h5,h6 { color: white !important}',
-             '.literallink:link { color: #3afdff !important; background-color: #008b8b !important; text-decoration: none !important}',
-             '.literallink:visited { color: #ff57fd !important; background-color: darkmagenta !important; text-decoration: none !important}'             ,
-             'em { background-color: #444444 !important;}',
-             // form elements
-             'input:focus, textarea:focus { '+
-             // + 'color: #dd4ca7 !important; '
-                 // 'background-color: blue !important; '+
-                 // 'border-color: red !important; '+
-                 'border: solid 1px red !important; '+
-                 // 'background-image: -moz-linear-gradient(top left, #5b9602 0%, #334502 100%) !important; '+
-                 ' }',
-             // submit button
-             'input[type="submit"]:hover { }',
-             'input[type="submit"]:focus { }',
+        ['*:not(img) {'+
+         // 'h1, h2, p, a, div, span, table, td, th tr, ul, li, strong, input, select, textarea, body {'+
+         'font-style: normal !important;'+ // italics is hard to read with gohu
+         'background: #303030 !important;'+
+         'color: #cccccc !important; '+
+         'text-shadow: 0px 0px 0px  black !important;'+ // fix ugly shadows
+         'letter-spacing: 0 !important;'+               // don't scrunch words
+         // '*:not(img) { background: #262626 !important; '+
+         // 'color: #f6f3e8 !important; '+
+         // 'color: #686868 !important; '+
+         // 'font-family: GohuFont, Tamsyn !important; '+
+         // 'font-family: OhSnap !important; '+
+         'font-family: Dina !important; '+
+         // 'font-family: DejaVu Sans !important; '+
+         // 'font-family: Inconsolata !important; '+
+         // 'font-family: MonteCarlo !important; '+
+         // 'font-family: Terminus !important; '+
+         // 'font-family: Envy Code R !important; '+
+         // 'font-size: 10pt !important; '+
+         'font-size: 12pt !important; '+
+         'padding: 1px !important;'+
+         '-moz-border-bottom-colors: #444 #444 !important; '+
+         '-moz-border-top-colors: #444 #444  !important; '+
+         '-moz-border-right-colors: #444 #444  !important; '+
+         '-moz-border-left-colors: #444 #444  !important; '+
+         'box-shadow: 0 0 0 black !important; '+
+         '-moz-box-shadow: 0 0 0 black !important; '+
+         'border-color: #444 !important; '+
+         'text-decoration: none !important;'+
+         // inventing on principles
+         // 'color: #abb1ad !important; '+
+         // 'background: #454545 !important;'+
+         ' }',
+         'p { line-height: 100% !important; }',
+         'code, pre, code *, pre * { color: #f6f3e8 !important; background-color: #353535 !important; }',
+         ':link, :link * {  color: #8ac6f2 !important}',
+         ':visited, :visited * { color: #95e454 !important}',
+         'h1,h2,h3,h4,h5,h6 { color: white !important}',
+         '.literallink:link { color: #3afdff !important; background-color: #008b8b !important; text-decoration: none !important}',
+         '.literallink:visited { color: #ff57fd !important; background-color: darkmagenta !important; text-decoration: none !important}'             ,
+         'em { background-color: #444444 !important;}',
+         // form elements
+         'input:focus, textarea:focus { '+
+         // + 'color: #dd4ca7 !important; '
+         // 'background-color: blue !important; '+
+         // 'border-color: red !important; '+
+         'border: solid 1px red !important; '+
+         // 'background-image: -moz-linear-gradient(top left, #5b9602 0%, #334502 100%) !important; '+
+         ' }',
+         // submit button
+         'input[type="submit"]:hover { }',
+         'input[type="submit"]:focus { }',
 
-             // youtube html5 player
-             'div#watch-video-container *, div#video-player-html5 * { background-color: transparent !important; }',
+         // youtube html5 player
+         'div#watch-video-container *, div#video-player-html5 * { background-color: transparent !important; }',
 
-             'span.__conkeror_hint {'+
-                 // 'background-color: #104e8b !important;'+
-                 'color: white !important;'+
-                 // + 'font-family: GohuFont !important;'
-                 // + 'line-height: 14px !important;'
-                 'font-size: 14px !important;'+
-                 '}'
+         // brown
+         // '*:not(img) { color: #eeddcc !important; '+
+         //     'background: #472d1e !important; }',
+         // ':link, :link * {  color: #66bbff !important}',
+         // ':visited, :visited * { color: #aabb00 !important}',
+         // 'h1,h2,h3,h4,h5,h6 { color: #aa8866 !important}',
+         // 'code, pre, code *, pre * { color: #ba9 !important; background-color: #1c1008 !important; }',
+
+         'span.__conkeror_hint {'+
+         // 'background-color: #104e8b !important;'+
+         'color: white !important;'+
+         // + 'font-family: GohuFont !important;'
+         // + 'line-height: 14px !important;'
+         'font-size: 14px !important;'+
+         '}'
          ], $url_prefixes = ["http", "about"]),
 
-     make_css_data_uri(['em { background-color: #444444 !important;}'], $url_prefixes = ["http://www.google.com/reader"]),
+     make_css_data_uri(
+         ['em { background-color: #444444 !important;}'
+         ], $url_prefixes = ["http://www.google.com/reader"]),
 
      make_css_data_uri(
          ['.vh { color: white; '+
@@ -144,7 +159,7 @@ global_color_theme(
           ' { display: none !important;}'
          ], $domains = "mail.google.com"),
 
-          make_css_data_uri(
+     make_css_data_uri(
          [':link img { border: dashed 1px #008b8b !important; margin: 5px !important; }',
           ':visited img { border: dashed 1px darkmagenta !important; margin: 5px !important; }'
          ], $domains = ["bbs.archlinux.org",
@@ -152,12 +167,16 @@ global_color_theme(
 
      make_css_data_uri(
          ['#entries { padding-left: 5px !important; padding-right: 5px !important; }', // don't add padding on left side
-          'div#current-entry div.card { border: solid 2px #777 !important;} ',
+          'div#current-entry div.card { border: solid 2px #8968cd !important;} ',
+          'div#current-entry div.card:-moz-window-inactive { border-color: #777 ! important; }',
+          '#message-area-container { display: none !important; }',
+          "#message-area-container div.goog-inline-block { display: none !important: }",
+          "div.section-minimize { display: none !important; }",
           '.entry { background-color: #444 !important; }',
           // hide
           '#viewer-footer, '+   // next prev buttons
           '#chrome-header, '+   // tag name, expanded/list
-          '#viewer-header, '+   // showing new items, mark and folder buttons
+          '#viewer-header-container, '+   // showing new items, mark and folder buttons
           '#top-bar,'+          // logo and search
           "#lhn-recommendations,"+       // explore sidebar
           "#lhn-friends,"+               // followers sidebar
@@ -166,14 +185,20 @@ global_color_theme(
           "#trends-selector,"+           // trends
           "#directory-selector,"+        // browse for stuff
           ".entry-actions,"+             // browse for stuff
-          '#gb,'+                        // google bar
+          "#gb,"+                        // google bar
           '#title-and-status-holder,'+   // all items, sort by
           '.card-actions,'+              // social links
           '#scroll-filler-recs-message'+ // no more articles message
           '{ display: none !important;}'
          ], $url_prefixes = ["http://www.google.com/reader",
                              "https://www.google.com/reader"]),
-     
+
+     make_css_data_uri(
+         [".current { border: solid 2px #8968cd !important; }"+
+          "td.default { border :solid 2px transparent !important; }"+
+          ".current-comment > td.default { border: solid 2px #8968cd !important; }"
+         ], $domains = ["news.ycombinator.com"]),
+
      make_css_data_uri(
          ['.k { font-weight: bold !important }', // keyword
           'a, h1, code, pre {text-shadow: 0px 0px 0px  black !important; }', // ugly shadows
@@ -189,23 +214,32 @@ global_color_theme(
           '.gd, .gd .x { color: #ff0080 !important }' // deleted line
          ], $domains = "github.com")]);
 
-global_color_theme("light", "L",
-                   [make_css_data_uri(['*:not(img) { background: #edebe8 ! important; color: #2c2d32 !important; font-family: MonteCarlo !important; }',
-                    ':link, :link * { color: #1856ba !important }',
-                    ':visited, :visited * { color: #95e454 !important }'])]);
+global_color_theme(
+    "light", "L",
+    [make_css_data_uri(
+        ['*:not(img) { background: #edebe8 ! important; color: #2c2d32 !important; font-family: MonteCarlo !important; }',
+         ':link, :link * { color: #1856ba !important }',
+         ':visited, :visited * { color: #95e454 !important }'])]);
 
-global_color_theme("grey", "A",
-                   [make_css_data_uri(['*:not(img) { background: #4c4c4c ! important; color: #cccccc !important }',
-                    ':link, :link * { color: #8ac6f2 !important }',
-                    ':visited, :visited * { color: #95e454 !important }'])]);
+global_color_theme(
+    "grey", "A",
+    [make_css_data_uri(
+        ['*:not(img) { background: #4c4c4c ! important; color: #cccccc !important }',
+         ':link, :link * { color: #8ac6f2 !important }',
+         ':visited, :visited * { color: #95e454 !important }'])]);
 
-// global_color_theme("grey", "A",
-//                    [make_css_data_uri(['*:not(img) { background: #535353 ! important; color: #ffffff !important }',
-//                     ':link, :link * { color: #8ac6f2 !important }',
-//                     ':visited, :visited * { color: #95e454 !important }'])]);
+// global_color_theme(
+//     "grey", "A",
+//     [make_css_data_uri(
+//         ['*:not(img) { background: #535353 ! important; color: #ffffff !important }',
+//          ':link, :link * { color: #8ac6f2 !important }',
+//          ':visited, :visited * { color: #95e454 !important }'])]);
 
-// global_color_theme("beige", "Q",
-//                    [make_css_data_uri(['*:not(img) { background: #f5deb3 ! important; color: #000000 !important; font-family: GohuFont, Tamsyn !important; font-size: 10pt !important; }',
-//                     ':link, :link * { color: #005f87 !important }',
-//                     ':visited, :visited * { color: #d75f00 !important }'])]);
+// global_color_theme(
+//     "beige", "Q",
+//     [make_css_data_uri(
+//         ['*:not(img) { background: #f5deb3 ! important; color: #000000 !important; font-family: GohuFont, Tamsyn !important; font-size: 10pt !important; }',
+//          ':link, :link * { color: #005f87 !important }',
+//          ':visited, :visited * { color: #d75f00 !important }'])]);
+
 
