93b93671efbb676b6779084986a5ee8cf0016653
Scott Jaderholm <jaderholm@gmail.com>
2012-12-20 00:42:52 -0800

    Use # keys instead of Fn keys for group switching in Stumpwm.

--- .stump/groupkeys.lisp
+++ .stump/groupkeys.lisp
@@ -1,3 +1,4 @@
+(in-package :stumpwm)
 ;;; M-s-F# to move window to group #
 (defcommand gmove-and-follow (to-group) ((:group "To Group: "))
   "Move the current window to the specified group and go there."
@@ -9,9 +10,9 @@
 (dotimes (i 6)
   (define-key
       *top-map*
-      (kbd (concatenate 'string "M-F" (write-to-string (1+ i))))
+      (kbd (concatenate 'string "s-" (write-to-string (1+ i))))
     (concatenate 'string "gselect " (write-to-string (1+ i))))
   (define-key
       *top-map*
-      (kbd (concatenate 'string "M-s-F" (write-to-string (1+ i))))
+      (kbd (concatenate 'string "M-s-" (write-to-string (1+ i))))
     (concatenate 'string "gmove-and-follow " (write-to-string (1+ i)))))
